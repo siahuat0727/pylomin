906e1391db9006cbb494da79ae66b8ee4c88de2f
siahuat0727 <tansiahuat@gmail.com>
2021-12-23 03:38:08 +0000

    Explicitly write device

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ do
 	for method in lazy-loading+chunked-embedding lazy-loading  naive lazy-loading+keep-embedding
 	do
 		# Run on GPU to trace the peak memory needed for the inference
-		python3 demo.py --batch_size $batch_size --method $method --device cuda
-		python3 demo.py --batch_size $batch_size --method $method --device cpu
+		python3 demo.py --batch_size $batch_size --method $method --device cuda --storage cpu
+		python3 demo.py --batch_size $batch_size --method $method --device cpu --storage disk
 	done
 done
